5f0ea5cb81276dbefe4456c03d2567334f32fa7a
jackarivera <jackarivera@gmail.com>
2022-08-08 12:27:30 -0500

    structure update

--- cerulean_sonar/rovmk2.py
+++ cerulean_sonar/rovmk2.py
@@ -1,10 +1,12 @@
-from asyncio.log import logger
+from cerulean_sonar.comm_serial import comm_serial
 import rclpy
 from rclpy.node import Node
 from brping import Ping1D
 from sensor_msgs.msg import Range
+import serial
+
 class rovmk2_sonar:
-    def __init__(self, params, sonar_node, autosyncEnabled):
+    def __init__(self, parameters, sonar_node, autosyncEnabled):
         # Handle master node
         global node   
         node = sonar_node
@@ -12,4 +14,20 @@ class rovmk2_sonar:
         # Setup logger
         global logger
         logger = sonar_node.get_logger()
-    
+
+        # Setup parameters
+        global params
+        params = parameters
+
+        # Create the global connection object
+        global comm
+
+        if params.get("comm_type") == "serial":
+            logger.info("Opening serial connection at port %s" % params.get("device_port"))
+            comm = comm_serial(params)
+            comm.openComm()
+            
+            if not comm.isOpen():
+                logger.info("Error. Serial communication is not open or could not be opened. Check device port and baudrate.\n")
+            else:
+                logger.info("Successfully connected to serial port ")
