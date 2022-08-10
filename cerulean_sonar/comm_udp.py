e9b02a1c525bf3435096c77581a6e9c99ddd9659
jackarivera <jackarivera@gmail.com>
2022-08-10 12:06:56 -0500

    add basic udp struct

--- cerulean_sonar/comm_udp.py
+++ cerulean_sonar/comm_udp.py
@@ -1,3 +1,5 @@
+import socket
+
 class comm_udp:
     def __init__(self, parameters):
         # Setup parameters
@@ -6,3 +8,13 @@ class comm_udp:
 
         # Create the global connection object
         global comm
+
+        comm = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        comm.bind((params.get("udp_address"), params.get("udp_port")))  
+    
+    def readByte(self):
+        data = comm.recvfrom(1024)
+        return data
+
+    def isOpen(self):
+        return comm.is_open
